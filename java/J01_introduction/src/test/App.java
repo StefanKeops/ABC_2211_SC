1fcf0971fb8190cf815e3111829ce747561d9bf7
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2022-12-19 10:32:52 +0100

    new

---
+++ java/J01_introduction/src/test/App.java
@@ -0,0 +1,27 @@
+package test;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// System.out = flux de sortie standard (affichage dans le terminal)
+		// System.in  = flux d'entrée (saisies utilisateur dans le terminal)
+		// System.err = flux de sortie d'erreur
+		
+	System.out.println("Hello world !");
+	
+	
+	String name;
+	String firstName = "Stefan Mircea";
+	
+	name = "Csaszar";
+	
+	System.out.println(name);
+	System.out.println(firstName);
+	
+	System.out.println("Bonjour " + firstName + " " + name);
+	
+	}
+
+}
