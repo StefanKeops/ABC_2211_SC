ca1a12c446d7e3173ea8e0431df658c4817b5bdf
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-01-06 10:34:50 +0100

    update 06/01/2023

--- java/J20_Jeu_de_la_fourchette/src/jeu_de_la_fourchette/App.java
+++ java/J20_Jeu_de_la_fourchette/src/jeu_de_la_fourchette/App.java
@@ -1,10 +1,46 @@
 package jeu_de_la_fourchette;
 
+/*VARIABLE
+ 
+ Générer aléatoirement un nombre entier compris entre 1 et 100 et le stocker dans une variable nombre_mystere
+Déclarer un tableau tentatives vide et une variable nb_essais initialisée à 0
+Tant que nombre_saisi != nombre_mystere
+  Demander à l'utilisateur de deviner le nombre mystère en saisissant un nombre entier et le stocker dans une variable nombre_saisi
+  Ajouter nombre_saisi au tableau tentatives et incrémenter nb_essais de 1
+  Si nombre_saisi < nombre_mystere
+    Afficher "Le nombre saisi est trop petit."
+  Sinon si nombre_saisi > nombre_mystere
+    Afficher "Le nombre saisi est trop grand."
+  Fin Si
+Fin Tant que
+Afficher "Bravo ! Vous avez trouvé le nombre mystère en nb_essais essais !"
+  
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+	    Random rand = new Random();
+	    int nombre_mystere = rand.nextInt(100) + 1;
+	    int nombre_saisi;
+	    ArrayList<Integer> tentatives = new ArrayList<>();
+	    int nb_essais = 0;
+	    do {
+	      System.out.println("Devinez le nombre mystère (entre 1 et 100) :");
+	      nombre_saisi = sc.nextInt();
+	      tentatives.add(nombre_saisi);
+	      nb_essais++;
+	      if (nombre_saisi < nombre_mystere) {
+	        System.out.println("Le nombre saisi est trop petit.");
+	      } else if (nombre_saisi > nombre_mystere) {
+	        System.out.println("Le nombre saisi est trop grand.");
+	      }
+	    } while (nombre_saisi != nombre_mystere);
+	    System.out.println("Bravo ! Vous avez trouvé le nombre mystère en " + nb_essais + " essais !");
+	  }
 	}
-
-}
