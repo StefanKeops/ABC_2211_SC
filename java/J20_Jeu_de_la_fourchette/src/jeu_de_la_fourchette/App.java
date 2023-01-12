817c92df6d19eaec395150cc7eb00b889385bf0f
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-01-12 16:20:47 +0100

    update 12/01/2023

--- java/J20_Jeu_de_la_fourchette/src/jeu_de_la_fourchette/App.java
+++ java/J20_Jeu_de_la_fourchette/src/jeu_de_la_fourchette/App.java
@@ -6,7 +6,7 @@ package jeu_de_la_fourchette;
  nombre_saisi = entier;
  nb_essais;
  
- Générer aléatoirement un nombre entier compris entre 1 et 100 et le stocker dans une variable nombre_Random
+ Générer aléatoirement un nombre entier compris entre 1 et 1000 et le stocker dans une variable nombre_Random
 Déclarer un tableau tentatives vide et une variable nb_essais initialisée à 0
 Tant que nombre_saisi != nombre_Random
   Demander à l'utilisateur de deviner le nombre Random en saisissant un nombre entier et le stocker dans une variable nombre_saisi
@@ -30,12 +30,12 @@ public class App {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 	    Random rand = new Random();
-	    int nombre_Random = rand.nextInt(100) + 1;
+	    int nombre_Random = rand.nextInt(1000) + 1;
 	    int nombre_saisi;
 	    ArrayList<Integer> tentatives = new ArrayList<>();
 	    int nb_essais = 0;
 	    do {
-	      System.out.println("Devinez le nombre Random (entre 1 et 100) :");
+	      System.out.println("Devinez le nombre Random (entre 1 et 1000) :");
 	      nombre_saisi = sc.nextInt();
 	      tentatives.add(nombre_saisi);
 	      nb_essais++;
