d787def2a4ea7993f216cd39e16fdcde711d540b
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-01-18 09:49:56 +0100

    update 18/01/2023

--- java/J32_tableau/src/tableau/App.java
+++ java/J32_tableau/src/tableau/App.java
@@ -5,56 +5,72 @@ public class App {
 		public static void main(String[] args) {
 			
 		int[] tabEntier = new int[] {1,2,3,4,5,6,7,8,9,10,11,12};
-		// double pi = Math.PI;
 		
 		// declaration d'un tableau de chaine de caractère
 		
-		String [] tabFigureGeometrique = new String[4];
+		String [] tabFigureGeometrique = new String[7];
 		
 		// stokage de valeur dans le tableau tabFigureGeometrique
 		
 		tabFigureGeometrique[0] = "Figure Geometrique";
-		tabFigureGeometrique[1] = "       Cerc       ";
-		tabFigureGeometrique[2] = "     Triangle     ";
-		tabFigureGeometrique[3] = "      Carré       ";
+		tabFigureGeometrique[1] = "------------------";
+		tabFigureGeometrique[2] = "      Cercle      ";
+		tabFigureGeometrique[3] = "     Triangle     ";
+		tabFigureGeometrique[4] = "      Carré       ";
+		tabFigureGeometrique[5] = "     Trapèze      ";
+		tabFigureGeometrique[6] = "    Rectangle     ";
 		
-		String [] tabRayon = new String[4];
+		String [] tabRayon = new String[7];
 		// stokage de valeur dans le tableau tabRayon (raza)
 		
-		tabRayon[0] = "Cotes - Rayon ";
-		tabRayon[1] = "    10 cm     ";
-		tabRayon[2] = "    10 cm     ";
-		tabRayon[3] = "    10 cm     ";
+		tabRayon[0] = "             Valeurs             ";
+		tabRayon[1] = "---------------------------------";
+		tabRayon[2] = "            r = 10 cm            ";
+		tabRayon[3] = "            l = 10 cm            ";
+		tabRayon[4] = "            l = 10 cm            ";
+		tabRayon[5] = " l = 10 cm, B = 15 cm, b = 10 cm ";
+		tabRayon[6] = "        l = 10 cm, L = 15        ";
 		
-		String [] tabDiametre = new String [4];
+		String [] tabDiametre = new String [7];
 		
 		// stokage de valeur dans le tableau tabDiametre (diametrul)
 		
 		tabDiametre[0] = "Hauteur - Diametre";
-		tabDiametre[1] = "        " + String.valueOf(2 * 10) + "        ";
-		tabDiametre[2] = " " + String.valueOf((10 * Math.sqrt(3)) / 4);
-		tabDiametre[3] = "      10   cm     ";
+		tabDiametre[1] = "------------------";
+		tabDiametre[2] = "        " + String.valueOf(2 * 10) + "        ";
+		tabDiametre[3] = " " + String.valueOf((10 * Math.sqrt(3)) / 4);
+		tabDiametre[4] = "        10        ";
+		tabDiametre[5] = "        " + String.valueOf(10 - 2.5) + "       ";
+		tabDiametre[6] = "        10        ";
 		
-		String [] tabAire = new String[4];
+		String [] tabAire = new String[7];
 		
 		// stokage de valeur dans le tableau tabAire (arie)
 		
 		tabAire[0] = "      Aire       ";
-		tabAire[1] = String.valueOf(Math.PI  * 10 * 10);
-		tabAire[2] = String.valueOf(10 * 10 * (Math.sqrt(3) / 4));
+		tabAire[1] = "-----------------";
+		tabAire[2] = String.valueOf(Math.PI  * 10 * 10);
+		tabAire[3] = String.valueOf(10 * 10 * (Math.sqrt(3) / 4));
+		tabAire[4] = "       " + String.valueOf(10 * 10) + "       ";
+		tabAire[5] = "       " +String.valueOf(((15+10) / 2) * 7.5) + "      ";
+		tabAire[6] = "       " + String.valueOf(15 * 10) + "       ";
 		
-		String [] tabPerimetre = new String[4];
+		String [] tabPerimetre = new String[7];
 		
 		// stokage de valeur dans le tableau tabPerimetre (perimetru)
 		
-		tabPerimetre[0] = "Perimetre";
-		tabPerimetre[1] = String.valueOf(2 * Math.PI * 10);
-		tabPerimetre[2] = String.valueOf(3 * 10);
+		tabPerimetre[0] = "     Perimetre   ";
+		tabPerimetre[1] = "-----------------";
+		tabPerimetre[2] = String.valueOf(2 * Math.PI * 10);
+		tabPerimetre[3] = "        " + String.valueOf(3 * 10) + "       ";
+		tabPerimetre[4] = "        " + String.valueOf(4 * 10) + "       ";
+		tabPerimetre[5] = "        " + String.valueOf(2 * 10 + 15 + 10 ) + "       ";
+		tabPerimetre[6] = "        " + String.valueOf((2 * 10) + (2 *15)) + "       ";
 		
 		// affichage de tout les éléments du tableau
 		for(int i = 0; i <=tabFigureGeometrique.length-1; i++)
 		{
-		System.out.println(i + " " + "|" + " " + tabFigureGeometrique[i] + " " + "|" + " " + tabRayon[i] + " " + "|" + " " + tabDiametre[i]+  " " + "cm" + " " + "|" + " " + tabAire[i]+  " " + "cm²" + " " + "|" + " " + tabPerimetre[i]+  " " + "cm");
+		System.out.println(i + " " + "|" + " " + tabFigureGeometrique[i] + " " + "|" + " " + tabRayon[i] + " " + "|" + " " + tabDiametre[i]+  " " + "cm" + " " + "|" + " " + tabAire[i]+  " " + "cm²" + " " + "|" + " " + tabPerimetre[i]+  " " + "cm" + "|");
 		}
 
 	}
