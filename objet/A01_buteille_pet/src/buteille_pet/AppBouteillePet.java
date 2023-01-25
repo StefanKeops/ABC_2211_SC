d9227a05cac38e674bcad9f5fc73bc7f9241b061
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-01-25 09:07:47 +0100

    25/01/2023

--- objet/A01_buteille_pet/src/buteille_pet/AppBouteillePet.java
+++ objet/A01_buteille_pet/src/buteille_pet/AppBouteillePet.java
@@ -18,12 +18,13 @@ public class AppBouteillePet {
 		boolean fermer;
 		boolean remplir;
 		boolean vider;
+		int volume1 = 750;
 		String coleur1 = "transparrente";
 		String materiel1 = "verre";
 		int masse1 = 704;
 		int hauteur1 = 26;
 		int diametreDuCol1 = 40;
-		int volume1 = 500;
+		int volume2 = 500;
 		String coleur11 = "noire";
 		String materiel11 = "inox";
 		int masse11 = 300;
@@ -33,14 +34,14 @@ public class AppBouteillePet {
 		
 	    buteille_choisi = sc.nextInt();
 		
-		if (buteille_choisi = 1) {
+	    if (buteille_choisi == 1) {
 			System.out.println("Buteille 1. caracteristiques : " + volume + " ml " + coleur + " avec un masse de " + masse + ", hauteur de " + hauteur + " et un diametre du col de " + diametreDuCol);
 	  
-		} else if (buteille_choisi = 3) {
-	    	  System.out.println("Buteille 3. caracteristiques : " + volume1 + " ml " + coleur11 + " avec un masse de " + masse11 + ", hauteur de " + hauteur1 + " et un diametre du col de " + diametreDuCol1);
-	      }
-	    while (buteille_choisi = 2); {
-	    	System.out.println("Buteille 2. caracteristiques : " + volume + " ml " + coleur1 + " avec un masse de " + masse1 + ", hauteur de " + hauteur1 + " et un diametre du col de " + diametreDuCol);
+		}  if  (buteille_choisi == 2) {
+			System.out.println("Buteille 2. caracteristiques : " + volume1 + " ml " + coleur1 + " avec un masse de " + masse1 + ", hauteur de " + hauteur1 + " et un diametre du col de " + diametreDuCol);
+	   	  	      }
+		if (buteille_choisi == 3) {
+	    	System.out.println("Buteille 3. caracteristiques : " + volume2 + " ml " + coleur11 + " avec un masse de " + masse11 + ", hauteur de " + hauteur1 + " et un diametre du col de " + diametreDuCol1);
           }		  
 
 	}
