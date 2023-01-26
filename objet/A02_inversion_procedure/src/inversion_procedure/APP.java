548f07a712f34bcc3fb62cc4901f321a53b65002
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-01-26 09:07:52 +0100

    update 26/01/2023

--- objet/A02_inversion_procedure/src/inversion_procedure/APP.java
+++ objet/A02_inversion_procedure/src/inversion_procedure/APP.java
@@ -1,31 +1,60 @@
 package inversion_procedure;
 
+/*
+PROCEDURE Inversion (VAL entier nombre1 , VAL entier b)
+
+    tmp <-- nombre1
+    nombre1 <-- nombre2
+    nombre2 <-- nombre1
+	
+	ecrire: "Aprés inversion : valeur de premier nombre : +nombre1+ valeur de deuxiemme nombre : +nombre2
+
+FIN PROCEDURE
+
+DEBUT PROGRAMME
+
+    nombre1 est un eniter
+    nombre2 est un entier
+    tmp est un entier
+
+    Ecrire "Veuillez saisir la premiere valeur"
+    Lire nombre1    
+ 	
+ 	Ecrire "Veuillez saisir la Seconde valeur"
+    lire nombre2    
+
+	Inversion( nombre1 , nombre2)
+
+FIN PROGRAMME
+*/
+
 import java.util.Scanner;
 
 public class APP {
 
-	public void inversion(int nombre1, int nombre2) 
+	public static void inversion(int nombre1, int nombre2) 
 	{
-		int resultat;
+		int tmp;
 		
-		int tmp = nombre1;
+		tmp = nombre1;
 		nombre1 = nombre2;
 		nombre2 = tmp;
 		
+        System.out.println("Aprés inversion : valeur de premier nombre : "+nombre1+" valeur de deuxiemme nombre : "+nombre2);
 	}
 	
 public static void main(String[] args) {
 		
-		int a;
-		int b;
+		int nombre1;
+		int nombre2;
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println ("Veuillez saisir le 1er nombre :");
-		a = sc.nextInt();
+		nombre1 = sc.nextInt();
 		System.out.println ("Veuillez saisir le 2eme nombre :");
-		b = sc.nextInt();
+		nombre2 = sc.nextInt();
 		
-		System.out.println("Le resultat est : "+inversion(a,b));
+		inversion(nombre1, nombre2);
 
 		sc.close();
 
