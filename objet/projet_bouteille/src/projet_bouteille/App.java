b5404132c59785cd7b5bbce757f032e525a05f67
StefanKeops <120395876+StefanKeops@users.noreply.github.com>
2023-01-30 16:53:12 +0100

    30/01/2023

--- objet/projet_bouteille/src/projet_bouteille/App.java
+++ objet/projet_bouteille/src/projet_bouteille/App.java
@@ -3,7 +3,33 @@ package projet_bouteille;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Bouteille monster = new Bouteille (550, 500, false, "Monster");
+		System.out.println(monster.toString());
+		
+		Boolean aReussi= monster.ouvrir();
+		if (aReussi==true){
+			System.out.println("La bouteille a pu etre ouverte");
+		}
+		else{
+			System.out.println("La bouteille n'a pas pu etre ouverte car elle etait déjà ouverte");
+		}
+		System.out.println(monster.toString());
+		
+		Boolean aReussi2= monster.ouvrir();
+		if (aReussi2==true){
+			System.out.println("La bouteille a pu etre ouverte");
+		}
+		else{
+			System.out.println("La bouteille n'a pas pu etre ouverte car elle etait déjà ouverte");
+		}
+		System.out.println(monster.toString());
+		
+		
+		// monster.fermer();
+		// monster.remplirTout();
+		//monster.remplir(45);
+		//monster.viderTout();
 
 	}
 
--- objet/projet_bouteille/src/projet_bouteille/Bouteille.java
+++ objet/projet_bouteille/src/projet_bouteille/Bouteille.java
@@ -7,32 +7,81 @@ public class Bouteille {
 	private double contenanceEnL;
 	private boolean estOuverte;
 	private String nom;
+
+	
+	// les constructeurs
+	
+	public Bouteille( double capaciteEnL , double contenanceEnL , boolean estOuverte , String nom ) {
+		this.capaciteEnL = capaciteEnL;
+		this.contenanceEnL = contenanceEnL;
+		this.estOuverte = estOuverte;
+		this.nom = nom;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return 	"capaciteEnL="+capaciteEnL+
+		" contenanceEnL="+contenanceEnL+
+		" estOuverte="+estOuverte+
+		" nom="+nom;
+	}
 	
 	// les methodes
-	
-	public static boolean ouvrir()
+		
+	public boolean ouvrir()
 	{
+	if (this.estOuverte == true) {
+		return false;
+			} else {
+				this.estOuverte = true;
+		return true;
+	}
 	
 	}
 	
-	public static boolean fermer()
+	 public boolean fermer()
 	{
+		 if (this.estOuverte == false) {
+			 System.out.println("la bouteille est ferme");
+			 return false;
+		 } else {
+			 System.out.println("La bouteille est ouverte");
+			 return true;
+	}
+	}
+	
+	public boolean remplirTout ()
+	{
+		if (contenanceEnL < capaciteEnL)
+			System.out.println ("La bouteille est ouverte et pleine");
+		return true;
+		 }
+        
+        
+	public boolean remplir(double quantiteEnL)
+	{
+		double plein = quantiteEnL + this.contenanceEnL;
+		if (this.estOuverte == true & plein > this.capaciteEnL) {
+			System.out.println ("La bouteille il est plein et s'est renverse");
+				return true;
+			} else if (this.estOuverte == true & plein < this.capaciteEnL) {
+				System.out.println ("La bouteille est ouverte et contient " + plein + " ml");
+				return true;
+			} else {
+				 System.out.println("la bouteille est ferme");
+				 return false;
 		
 	}
 	
-	public static boolean remplirTout()
+	/*public boolean viderTout()
 	{
-		
-	}
+		if (contenanceEnL > 0)
+			return true
 	
-	public static boolean remplir(double quantiteEnL)
-	{
-		
-	}
-	
-	public static boolean vidertout()
-	{
-		
+	} else {
+			return false
+			
 	}
 	
 	public static boolean vider(double quantiteEnL)
@@ -61,6 +110,6 @@ public class Bouteille {
 		this.contenanceEnL = _contenanceEnL;
 		this.estOuverte = _estOuverte;
 		this.nom = _Nom;
-	}
+	}*/
 
-}
+	}}
